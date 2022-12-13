4ce0907c4b6f2d1a502dffc1422744031d77b9c4
AG4lyf <surajbhari159@gmail.com>
2022-12-13 19:20:24 +0530

    add question number 5

--- C++ File/Q5.cpp
+++ C++ File/Q5.cpp
@@ -7,7 +7,66 @@
 */
 #include<iostream>
 
+using namespace std;
 int main()
 {
-    
+    cout << "Enter number of matrix" << endl;
+    int x;
+    int each;
+    cin >> x;
+    cout << "Enter number of elements in each row" <<endl;
+    cin >> each;
+    int i = 0;
+    int j = 0;
+    int a[x][each];
+    cout << "Enter elements of matrix" << endl;
+    for(i = 0; i < x; i++)
+    {
+        for(j = 0; j < each; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    cout << "Matrix is" << endl;
+    for(i = 0; i < x; i++)
+    {
+        for(j = 0; j < each; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Spiral form of matrix is" << endl;
+    int k = 0;
+    int l = 0;
+    while(k < x && l < each)
+    {
+        for(i = l; i < each; i++)
+        {
+            cout << a[k][i] << " ";
+        }
+        k++;
+        for(i = k; i < x; i++)
+        {
+            cout << a[i][each - 1] << " ";
+        }
+        each--;
+        if(k < x)
+        {
+            for(i = each - 1; i >= l; i--)
+            {
+                cout << a[x - 1][i] << " ";
+            }
+            x--;
+        }
+        if(l < each)
+        {
+            for(i = x - 1; i >= k; i--)
+            {
+                cout << a[i][l] << " ";
+            }
+            l++;
+        }
+    }
+    return 0;
 }
